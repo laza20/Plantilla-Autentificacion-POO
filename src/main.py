fa9cc29e4c9db8477fadcddeaa7512a2f18235d4
lazaro20 <salvirialazaro@gmail.com>
2026-06-08 14:32:12 -0300

    Feat(src/main.py): Actualizar el main que de manera efectiva: Consuma los routers desde la ubicacion correcta. Permita a las excepciones cumplir su labor.

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+load_dotenv()
+from fastapi import FastAPI, Request
+from sqlmodel import SQLModel
+from src.database.client import engine
+from exceptions.domain import DomainError
+from fastapi.responses import JSONResponse
+
+SQLModel.metadata.create_all(engine)
+app = FastAPI()
+
+from src.auth.routers import(
+    usuarios
+)
+
+app.include_router(usuarios.router)
+
+
+
+@app.exception_handler(DomainError)
+async def domain_error_handler(request: Request, exc: DomainError):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message}
+    )
