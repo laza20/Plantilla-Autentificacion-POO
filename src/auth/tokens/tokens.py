bfec8ac537e44120a09f4151d0df5c1d074f3252
lazaro20 <salvirialazaro@gmail.com>
2026-06-08 16:04:36 -0300

    Feat(src/auth/tokens/): Crear el archivo de tokens el cual se encarga de: Crear el acces token (token de corta duracion). Crear el refresh token (token de larga duracion). Refresh token (funcion encargada de refrescar el access token por medio del refresh token).

---
+++ src/auth/tokens/tokens.py
@@ -0,0 +1,57 @@
+from datetime import timedelta
+from src.auth.security import security
+from fastapi import Body
+from exceptions.usuarios_exceptions import TokenInvalido
+from jose import JWTError
+from config.config import settings
+
+
+def create_access_token(user_id: str) -> str:
+    """
+    Funcion encargada de crear un access token para un usuario dado su id.
+    El access token tiene una duración corta y se utiliza para autenticar las solicitudes del usuario.
+    """
+    return security.encode_token(
+        {
+            "sub": user_id,
+            "type": "access"
+        },
+        timedelta(minutes=settings.ACCESS_TOKEN_DURATION)
+    )
+
+
+def create_refresh_token(user_id: str) -> str:
+    """
+    Funcion encarcargada de crear un refresh token para un usuario dado su id.
+    El refresh token tiene una duración más larga que el access token y se utiliza para obtener nuevos
+    """
+    return security.encode_token(
+        {
+            "sub": user_id,
+            "type": "refresh"
+        },
+        timedelta(days=settings.REFRESH_TOKEN_DURATION)
+    )
+
+
+def refreshed_token(refresh_token: str = Body(...)):
+    """
+    Servicio para refrescar el access token usando un refresh token válido.
+    """
+    try:
+        payload = security.decode_token(refresh_token)
+
+        user_id = payload.get("sub")
+        if not user_id:
+            raise TokenInvalido("Refresh token inválido")
+
+    except JWTError:
+        raise TokenInvalido("Refresh token inválido o expirado")
+
+    new_access_token = create_access_token(user_id)
+
+    return {
+        "access_token": new_access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer"
+    }
