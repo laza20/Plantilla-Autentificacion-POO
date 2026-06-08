ff6df06f69d30c96b062e18f81929fc33583e5c3
lazaro20 <salvirialazaro@gmail.com>
2026-06-08 16:06:48 -0300

    Feat(exceptions/usuarios_exceptions.py): Crear las excepciones de usuario.

---
+++ exceptions/usuarios_exceptions.py
@@ -0,0 +1,52 @@
+from exceptions.domain import DomainError
+
+class UsuarioError(DomainError):
+    status_code = 400
+    pass
+
+class LoginError(UsuarioError):
+    status_code = 400
+    pass
+
+class UsuarioNoEncontrado(UsuarioError):
+    status_code = 409
+    pass
+
+class NoAutenticado(UsuarioError):
+    status_code = 401
+    pass
+
+class SinAccessToken(UsuarioError):
+    status_code = 401
+    pass
+
+class SinRefreshToken(UsuarioError):
+    status_code = 401
+    pass
+
+class TokenInvalido(UsuarioError):
+    status_code = 401
+    pass
+
+class UsuariosNoEncontrados(UsuarioError):
+    status_code = 409
+    pass
+
+
+class ResultadoInvalido(DomainError):
+    status_code = 409
+    pass
+
+class AvatarError(UsuarioError):
+    status_code = 409
+    pass
+
+class TiempoInterrupcionInicioSesion(UsuarioError):
+    status_code = 403
+    pass
+
+
+class UsuarioInactivo(UsuarioError):
+    status_code = 423
+    pass
+
