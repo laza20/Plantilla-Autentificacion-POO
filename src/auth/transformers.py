63b6ca4ff6f6f4fa7aa309d30628bb4d6bd2d03f
lazaro20 <salvirialazaro@gmail.com>
2026-06-08 16:05:13 -0300

    Feat(src/auth/transformers.py): Crear un transformador basico para la carga de un usuairo con su imagen.

---
+++ src/auth/transformers.py
@@ -0,0 +1,14 @@
+from fastapi import Form, File, UploadFile
+from datetime import date
+from typing import Optional
+
+async def parse_usuario_form(
+    email: str = Form(...),
+    password: str = Form(...),
+    imagen_url: Optional[UploadFile] = File(default=None)
+):
+    return {
+        "email": email,
+        "password_hash": password,
+        "imagen_url": imagen_url,
+    }
