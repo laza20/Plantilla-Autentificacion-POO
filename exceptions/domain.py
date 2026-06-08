70a5af1f05faf322f84dfef98800736156a26b94
lazaro20 <salvirialazaro@gmail.com>
2026-06-08 16:07:12 -0300

    Feat(exceptions/domain.py): Agregar excepciones de dominio.

--- exceptions/domain.py
+++ exceptions/domain.py
@@ -5,3 +5,16 @@ class DomainError(Exception):
         self.message = message
         self.status_code = status_code or getattr(self, "status_code", 400)
         super().__init__(self.message)
+
+class ContraseñaNoSegura(DomainError):
+    status_code = status.HTTP_400_BAD_REQUEST
+
+    def __init__(self, message: str = "La contraseña no cumple con los requisitos de seguridad."):
+        super().__init__(message)
+
+
+class SinCargas(DomainError):
+    status_code = status.HTTP_409_CONFLICT
+    
+    def __init__(self, message: str = "No se han realizado cargas."):
+        super().__init__(message)
