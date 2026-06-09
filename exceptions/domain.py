5f28741f336958a394122f9c4e659975821487e4
lazaro20 <salvirialazaro@gmail.com>
2026-06-09 14:05:04 -0300

    Feat(exceptions/domain.py): Agregar excepciones de dominio.

--- exceptions/domain.py
+++ exceptions/domain.py
@@ -18,3 +18,25 @@ class SinCargas(DomainError):
     
     def __init__(self, message: str = "No se han realizado cargas."):
         super().__init__(message)
+
+class LongitudExcedida(DomainError):
+    status_code = status.HTTP_422_UNPROCESSABLE_CONTENT
+    def __init__(self, message: str = "Uno o más campos exceden el límite de caracteres permitido."):
+        super().__init__(message)
+
+
+class LimiteTamañoSuperado(DomainError):
+    status_code = status.HTTP_413_REQUEST_ENTITY_TOO_LARGE
+    def __init__(self, message: str = "El tamaño de la imagen excede el límite permitido."):
+        super().__init__(message)
+
+class ExtensionNoPermitida(DomainError):
+    status_code = status.HTTP_400_BAD_REQUEST
+    def __init__(self, message: str = "La extensión del archivo no está permitida."):
+        super().__init__(message)
+
+class ErrorCloudinary(DomainError):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    def __init__(self, error: str, message: str = "Error al subir la imagen a Cloudinary."):
+        full_message = f"{message} Detalles del error: {error}"
+        super().__init__(full_message)
