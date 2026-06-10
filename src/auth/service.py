82bd939a4ce2ce729c30fa7020ce90d31bc9cc62
lazaro20 <salvirialazaro@gmail.com>
2026-06-10 14:11:06 -0300

    Feat(src/auth/service.py): Crear dos funciones, las cuales se encargan de: Logear a un usuario y retornar sus credenciales. Funcion interna que obtiene los tokens y prepara el retorno al end point.

--- src/auth/service.py
+++ src/auth/service.py
@@ -4,11 +4,13 @@ from src.auth.utils import usuarios_utils
 from typing import Dict
 from jose import JWTError
 from src.exceptions.domain import SinCargas, ContraseñaNoSegura
-from src.exceptions.usuarios_exceptions import TokenInvalido, UsuarioNoEncontrado
+from src.exceptions.usuarios_exceptions import TokenInvalido, UsuarioNoEncontrado, UsuarioError, LoginError, TiempoInterrupcionInicioSesion
+from src.exceptions.domain import DomainError
 from src.auth import repository 
 from src.cloudinary.cloudinary import insertar_imagen
-from src.auth.security.security import hash_password, decode_token
+from src.auth.security.security import hash_password, decode_token, verify_password
 from src.auth.tokens.tokens import create_access_token, create_refresh_token
+from datetime import datetime, timezone
 import logging
 
 logger = logging.getLogger(__name__)
@@ -67,3 +69,37 @@ def verificar_mail(token: str, session: Session):
         "refresh_token": create_refresh_token(str(usuario.id_usuario))
     }
     return tokens
+
+
+def login_with_credentials(mail: str, password: str, session: Session):
+    try:
+        usuario_db = repository.buscar_y_retornar_user_por_mail_username(session, mail)
+        usuario = usuario_db.model_dump()
+            
+        if not verify_password(password, usuario["password"]):
+            raise LoginError("Usuario o contraseña incorrectos")
+
+        return _emitir_tokens_usuario(usuario)
+    
+    except (UsuarioError, DomainError):
+        raise
+    except Exception as e:
+        logger.exception("Error crítico en service")
+        raise UsuarioError("Error al inicial sesion") from e
+    
+    
+def _emitir_tokens_usuario(dict_usuario:Dict)->Dict:
+        access_token = create_access_token(dict_usuario["id_usuario"])
+        refresh_token = create_refresh_token(dict_usuario["id_usuario"])
+        usuario_publico = usuarios_utils.retornar_usuario_publico(
+            dict_usuario.get("email"), 
+            dict_usuario.get("imagen_url")
+            )
+
+
+        return {
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+            "token_type": "bearer",
+            "usuario": usuario_publico
+        }
