4913cbcc1f44eabe104edda80eebd7feb6b2fe04
lazaro20 <salvirialazaro@gmail.com>
2026-06-19 16:29:37 -0300

    Refactor(src/cloudinary/cloudinary.py): Modificar la estructura de la api para migrar hacia la utilizacion de POO y reglas SOLID dentro de la plantilla (todavia no funcional).

--- src/cloudinary/cloudinary.py
+++ src/cloudinary/cloudinary.py
@@ -3,6 +3,8 @@ from src.config.config import settings
 from src.auth.models import AuthUser
 from src.exceptions.domain import LimiteTamañoSuperado, ExtensionNoPermitida, ErrorCloudinary
 from src.cloudinary.cloudinary_config import cloudinary_uploader
+from src.config.config import Settings, get_settings
+from fastapi import Depends
 
 app = FastAPI(title="FastAPI Cloudinary Integration")
 
@@ -18,57 +20,75 @@ En caso de que mas servicios necesiten subir imagenes, se puede agregar el nombr
 SERVICIOS_VALIDOS y luego utilizar la función subir_imagen para subir la imagen al servicio 
 correspondiente.
 """
-def insertar_imagen(objeto: AuthUser, servicio:str)-> AuthUser:
-    """
-    Funcion encargada de eniar los datos a la funcion principal de subida de imagenes y actualizar el objeto con la URL optimizada y el public_id.
-     - Recibe un objeto que contiene el campo imagen_url con el archivo a subir y el servicio al que pertenece la imagen.
-     - Retorna el mismo objeto con los campos imagen_url e imagen_public_id actualizados con
-    """
-    subir_imagen_resultado = subir_imagen(servicio=servicio, file=objeto.imagen_url)
-    objeto.imagen_url = subir_imagen_resultado["url_optimizada"]
-    objeto.imagen_public_id = subir_imagen_resultado["public_id"]
-    return objeto
 
-def subir_imagen(
-    servicio: str = Query(..., description="El módulo al que pertenece la imagen"), 
-    file: UploadFile = File(...)
-    ):  
-    """
-    Funcion encargada de subir una imagen a Cloudinary, validando el servicio y la extensión del archivo.
-     - Recibe el servicio al que pertenece la imagen y el archivo a subir.
-    """
-    if servicio.lower() not in SERVICIOS_VALIDOS:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Servicio no válido. Opciones permitidas: {', '.join(SERVICIOS_VALIDOS)}"
-        )
+class ImageService:
+    def __init__(
+        self,
+        settings: Settings
+    ):
+        self.settigs = settings
 
-    extensiones_permitidas = ["jpg", "jpeg", "png", "webp"]
-    file_ext = file.filename.split(".")[-1].lower() if "." in file.filename else ""
+    def insertar_imagen(self, objeto: AuthUser, servicio:str)-> AuthUser:
+        """
+        Funcion encargada de eniar los datos a la funcion principal de subida de imagenes y actualizar el objeto con la URL optimizada y el public_id.
+        - Recibe un objeto que contiene el campo imagen_url con el archivo a subir y el servicio al que pertenece la imagen.
+        - Retorna el mismo objeto con los campos imagen_url e imagen_public_id actualizados con
+        """
+        subir_imagen_resultado = self.subir_imagen(servicio=servicio, file=objeto.imagen_url)
+        objeto.imagen_url = subir_imagen_resultado["url_optimizada"]
+        objeto.imagen_public_id = subir_imagen_resultado["public_id"]
+        return objeto
 
-    if file.size > MAX_FILE_SIZE:
-        raise LimiteTamañoSuperado()
+    def subir_imagen(
+        self,
+        servicio: str = Query(..., description="El módulo al que pertenece la imagen"), 
+        file: UploadFile = File(...)
+        ):  
+        """
+        Funcion encargada de subir una imagen a Cloudinary, validando el servicio y la extensión del archivo.
+        - Recibe el servicio al que pertenece la imagen y el archivo a subir.
+        """
+        if servicio.lower() not in SERVICIOS_VALIDOS:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Servicio no válido. Opciones permitidas: {', '.join(SERVICIOS_VALIDOS)}"
+            )
 
-    if file_ext not in extensiones_permitidas:
-        raise ExtensionNoPermitida()
+        extensiones_permitidas = ["jpg", "jpeg", "png", "webp"]
+        file_ext = file.filename.split(".")[-1].lower() if "." in file.filename else ""
 
-    try:
-        carpeta_destino = f"{settings.NOMBRE_APP}/{servicio.lower()}"
+        if file.size > MAX_FILE_SIZE:
+            raise LimiteTamañoSuperado()
 
-        resultado = cloudinary_uploader.upload(
-            file.file,
-            folder=carpeta_destino,
-            upload_preset=settings.CLOUDINARY_UPLOAD_PRESET,
-            resource_type="image"
-        )
+        if file_ext not in extensiones_permitidas:
+            raise ExtensionNoPermitida()
 
-        return {
-            "mensaje": f"Imagen subida con éxito al módulo de {servicio.lower()}",
-            "public_id": resultado.get("public_id"),
-            "url_optimizada": resultado.get("secure_url"),
-            "formato": resultado.get("format"),
-            "tamaño_bytes": resultado.get("bytes")
-        }
-        
-    except Exception as e:
-        raise ErrorCloudinary(error=str(e))
+        try:
+            carpeta_destino = f"{self.settings.NOMBRE_APP}/{servicio.lower()}"
+
+            resultado = cloudinary_uploader.upload(
+                file.file,
+                folder=carpeta_destino,
+                upload_preset=self.settings.CLOUDINARY_UPLOAD_PRESET,
+                resource_type="image"
+            )
+
+            return {
+                "mensaje": f"Imagen subida con éxito al módulo de {servicio.lower()}",
+                "public_id": resultado.get("public_id"),
+                "url_optimizada": resultado.get("secure_url"),
+                "formato": resultado.get("format"),
+                "tamaño_bytes": resultado.get("bytes")
+            }
+            
+        except Exception as e:
+            raise ErrorCloudinary(error=str(e))
+
+
+
+def get_image_service(
+    settings: Settings = Depends(get_settings)
+):
+    return ImageService(
+        settings
+    )
