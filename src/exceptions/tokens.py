fad048ddcd6b8c343510459db659bf44c2b91c04
lazaro20 <salvirialazaro@gmail.com>
2026-06-22 13:52:04 -0300

    Feat(src/exceptions/tokens.py): Crear excepciones para las cookies.

---
+++ src/exceptions/tokens.py
@@ -0,0 +1,31 @@
+from src.exceptions.domain import DomainError
+from fastapi import status
+
+class TokenException(DomainError):
+    def __init__(self, message: str, status_code: int = None):
+        self.message = message
+        self.status_code = status_code or getattr(self, "status_code", 400)
+        super().__init__(self.message)
+
+
+class TokenExpirado(TokenException): 
+    status_code = status.HTTP_403_FORBIDDEN
+    def __init__(self, message: str = "El token que desea utilizar a expirado"):
+        super().__init__(message)
+
+class VerificacionExpirada(TokenException): 
+    status_code = status.HTTP_403_FORBIDDEN
+    def __init__(self, message: str = "El enlace de verificación ha expirado"):
+        super().__init__(message)
+
+
+class TokenInvalido(TokenException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    def __init__(self, message: str = "Token de ingreso invalido."):
+        super().__init__(message)
+
+
+class VerificacionInvalida(TokenException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    def __init__(self, message: str = "El usuario NO ah sido identificado con exito."):
+        super().__init__(message)
