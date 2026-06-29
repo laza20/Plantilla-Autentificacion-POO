aea52cbd7c8927576513ae27adb5a15923ccad9d
lazaro20 <salvirialazaro@gmail.com>
2026-06-29 15:33:04 -0300

    Refactor(src/container/dependencies.py src/container/auth_container.py): Agregar el modelo de caso de uso de registro al sistema de dependencias.

--- src/container/auth_container.py
+++ src/container/auth_container.py
@@ -1,12 +1,12 @@
 from src.config.config import Settings
 from src.auth.utils.usuarios_utils import UserMapper
-from src.auth.cookies.cookies import  CookiesService
-from src.auth.service import AuthService
-from src.domain.protocols.user_repository import UserRepositoryProtocol
-from src.domain.protocols.token_service import TokenProtocol
-from src.domain.protocols.password_service import PasswordProtocol
-from src.domain.protocols.mail_service import MailProtocol
-from src.domain.protocols.image_service import ImageProtocol
+from src.auth.domain.protocols.user_repository import UserRepositoryProtocol
+from src.auth.domain.protocols.token_service import TokenProtocol
+from src.auth.domain.protocols.password_service import PasswordProtocol
+from src.auth.domain.protocols.mail_service import MailProtocol
+from src.auth.domain.protocols.image_service import ImageProtocol
+from src.auth.domain.services.password_policy import PasswordPolicyService
+from src.auth.use_cases.register import RegisterUseCase
 
 
 class AuthContainer:
@@ -18,7 +18,8 @@ class AuthContainer:
         token_service: TokenProtocol,
         password_service: PasswordProtocol, 
         mail_service: MailProtocol,        
-        image_service: ImageProtocol       
+        image_service: ImageProtocol,
+        password_policy: PasswordPolicyService,
     ):
         self.settings = settings
         self.repository = repository
@@ -26,13 +27,18 @@ class AuthContainer:
         self.password_service = password_service
         self.mail_service = mail_service
         self.image_service = image_service
-        self.auth_service = AuthService(
-            token_service=token_service,
-            password_service=password_service,
-            mail_service=mail_service,
-            image_service=image_service,
-            user_mapper=UserMapper(),
-            cookies=CookiesService(settings),
-            user_repository=repository,
-            settings=settings
+        self.password_policy = password_policy
+        
+    @property
+    def register_use_case(self) -> RegisterUseCase:
+
+        return RegisterUseCase(
+            settings=self.settings,
+            user_repository=self.repository,
+            password_service=self.password_service,
+            password_policy=self.password_policy,
+            mail_service=self.mail_service,
+            image_service=self.image_service,
+            token_service=self.token_service,
+            
         )
--- src/container/dependencies.py
+++ src/container/dependencies.py
@@ -4,14 +4,17 @@ from src.config.config import (Settings, get_settings)
 from src.container.auth_container import AuthContainer
 from src.auth.service import AuthService
 
-from src.domain.protocols.token_service import TokenProtocol
-from src.domain.protocols.password_service import PasswordProtocol
-from src.domain.protocols.mail_service import MailProtocol
-from src.domain.protocols.image_service import ImageProtocol
+from src.auth.domain.protocols.token_service import TokenProtocol
+from src.auth.domain.protocols.password_service import PasswordProtocol
+from src.auth.domain.protocols.mail_service import MailProtocol
+from src.auth.domain.protocols.image_service import ImageProtocol
 from src.auth.tokens.tokens import TokenService
 from src.auth.security.security import PasswordService
 from src.infrastructure.mail.mail import MailService
 from src.infrastructure.images.cloudinary import ImageService
+from src.auth.domain.services.password_policy import PasswordPolicyService
+from src.auth.use_cases.register import RegisterUseCase
+
 
 
 
@@ -38,6 +41,9 @@ def get_image_service(
 ) -> ImageProtocol:
     return ImageService(settings)
 
+def get_password_policy(settings: Settings = Depends(get_settings)) -> PasswordPolicyService:
+    return PasswordPolicyService()
+
 
 def get_auth_service(
     settings: Settings = Depends(get_settings),
@@ -56,3 +62,23 @@ def get_auth_service(
         mail_service=mail_service,
         image_service=image_service
     ).auth_service
+
+def get_register_use_case(
+    settings: Settings = Depends(get_settings),
+    repository: UserRepository = Depends(get_user_repository),
+    token_service: TokenProtocol = Depends(get_token_service),
+    password_service: PasswordProtocol = Depends(get_password_service),
+    password_policy: PasswordPolicyService = Depends(get_password_policy),
+    mail_service: MailProtocol = Depends(get_mail_service),
+    image_service: ImageProtocol = Depends(get_image_service)
+) -> RegisterUseCase:
+
+    return AuthContainer(
+        settings=settings,
+        repository=repository,
+        token_service=token_service,
+        password_service=password_service,
+        password_policy=password_policy,
+        mail_service=mail_service,
+        image_service=image_service
+    ).register_use_case
