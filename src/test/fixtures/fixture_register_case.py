f061bc7123d136182b812f181d95dc9e59f43cea
lazaro20 <salvirialazaro@gmail.com>
2026-07-10 16:09:55 -0300

    Feat(src/test/fixtures/fixture_register_case.py): Agregar un fixture el cual permite desacoplar la inicializacion de un test.

---
+++ src/test/fixtures/fixture_register_case.py
@@ -0,0 +1,33 @@
+from src.test.unit.auth.domain.service import (
+    stub_image_service,
+    stub_mail_service,
+    stub_password_service,
+    stub_token_service
+)
+from src.test.unit.auth.domain.fakes.fake_user_repository import FakeUserRepository
+from src.auth.application.use_cases.register import RegisterUseCase
+from src.auth.domain.services.password_policy import PasswordPolicyService
+from src.test.config.config import TestSettings
+
+
+class RegisterTestEnvironment:
+
+    def __init__(self):
+        self.user_repository = FakeUserRepository()
+        self.password_service = stub_password_service.StubPasswordService()
+        self.token_service = stub_token_service.StubTokenService()
+        self.mail_service = stub_mail_service.StubMailService()
+        self.image_service = stub_image_service.StubImageService()
+        self.password_policy = PasswordPolicyService()
+        self.settings = TestSettings()
+
+    def use_case(self):
+        return RegisterUseCase(
+            user_repository=self.user_repository,
+            password_service=self.password_service,
+            mail_service=self.mail_service,
+            image_service=self.image_service,
+            token_service=self.token_service,
+            password_policy=self.password_policy,
+            settings=self.settings,
+        )
