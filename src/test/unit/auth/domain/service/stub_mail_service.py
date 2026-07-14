24ca6578c0108fc6b5bdf25ecd7550847110db9a
lazaro20 <salvirialazaro@gmail.com>
2026-07-14 16:58:02 -0300

    Refactor(src/test/unit/auth/domain/service/stub_mail_service.py): Modificar la composicion del service de mail para utilizarlo correctamente.

--- src/test/unit/auth/domain/service/stub_mail_service.py
+++ src/test/unit/auth/domain/service/stub_mail_service.py
@@ -1,15 +1,15 @@
 class StubMailService:
     def __init__(self):
-        pass
+        self.urls_list = []
 
     async def enviar_mail(self, email_destino: str, cuerpo_html: str, asunto: str) -> None:
         """
         Función para simular el envío de un correo electrónico.
         """
-        pass
+        self.urls_list.append(cuerpo_html)
 
     def generar_correo_verificacion(self, url: str, nombre_proyecto: str) -> str:
         """
         Función para simular la generación de un correo de verificación.
         """
-        return "correo-generado"
+        return url
