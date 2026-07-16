81f72f1cb7ac2d9d2766550328e9b744ce1af05f
lazaro20 <salvirialazaro@gmail.com>
2026-07-16 14:40:36 -0300

    Refactor: Refactorizar los stubs para utilizar parametros internos de manera que los test aporten comportamiento.

--- src/test/unit/auth/domain/service/stub_image_service.py
+++ src/test/unit/auth/domain/service/stub_image_service.py
@@ -3,12 +3,16 @@ from src.auth.infrastructure.persistence.postgres.models import AuthUser
 
 class StubImageService:
     def __init__(self):
-        pass
+        self.fue_llamado = False
+        self.imagen_recibida = None
+        self.servicio_recibido = None
 
     def insertar_imagen(self, objeto_usuario: AuthUser, imagen: UploadFile, servicio="usuarios") -> AuthUser:
         """
         Función para simular la inserción de una imagen.
         """
-        # Simula la inserción de la imagen y retorna un nombre de archivo ficticio
         objeto_usuario.imagen_url = f"{servicio}_imagen_ficticia.jpg"
+        self.fue_llamado = True
+        self.imagen_recibida = imagen
+        self.servicio_recibido = servicio
         return objeto_usuario
--- src/test/unit/auth/domain/service/stub_token_service.py
+++ src/test/unit/auth/domain/service/stub_token_service.py
@@ -1,8 +1,14 @@
 class StubTokenService:
+    def __init__(self):
+        self.fue_llamado = False
+        self.user_id_recibido = None
+        self.token_generado = None
 
     def create_access_token(self, user_id: int) -> str:
         """
         Función para simular la creación de un token de acceso.
         """
-        return f"access_token_{user_id}"
-
+        self.fue_llamado = True
+        self.user_id_recibido = user_id
+        self.token_generado = f"access_token_{user_id}"
+        return self.token_generado
