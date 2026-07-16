b0eb1c8ccc6bff5f4e4333b43c63ab4980a3f5c5
lazaro20 <salvirialazaro@gmail.com>
2026-07-16 10:51:08 -0300

    Refactor(src/test/unit/auth/domain/service/stub_mail_service.py): Utilizar parametros lineales en lugar de listas.

--- src/test/unit/auth/domain/service/stub_mail_service.py
+++ src/test/unit/auth/domain/service/stub_mail_service.py
@@ -1,12 +1,18 @@
 class StubMailService:
     def __init__(self):
-        self.urls_list = []
+        self.fue_llamado = False
+        self.destinatario = None
+        self.asunto = None
+        self.cuerpo_html = None
 
     async def enviar_mail(self, email_destino: str, cuerpo_html: str, asunto: str) -> None:
         """
         Función para simular el envío de un correo electrónico.
         """
-        self.urls_list.append(cuerpo_html)
+        self.fue_llamado = True
+        self.destinatario = email_destino
+        self.asunto = asunto
+        self.cuerpo_html = cuerpo_html
 
     def generar_correo_verificacion(self, url: str, nombre_proyecto: str) -> str:
         """
