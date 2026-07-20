d594e2ba74714e5d7a0e20958ca5d876cd8bdeba
lazaro20 <salvirialazaro@gmail.com>
2026-07-20 16:01:46 -0300

    Test: Crear los test necesarios para cubrir todos los casos posibles de error dentro de contraseña no segura.

--- src/test/unit/auth/domain/use_case/test_password_policy.py
+++ src/test/unit/auth/domain/use_case/test_password_policy.py
@@ -1,20 +1,39 @@
 import pytest
 from src.auth.domain.exceptions.domain import ContraseñaNoSegura
-from src.auth.infrastructure.persistence.postgres.models import UserRegisterDTO
-from src.test.fixtures.fixture_register_case import RegisterTestEnvironment
+from src.auth.domain.services.password_policy import PasswordPolicyService
 
 
-@pytest.mark.asyncio
-async def test_no_debe_registrar_un_usuario_con_password_sin_numeros():
+def test_debe_dar_error_por_contraseña_sin_numeros():
     """
-    Verifica que el registro rechaza usuarios cuya contraseña no cumple la política de seguridad por falta de numeros.
+    Verifica que la contraseña no cumple con la política de seguridad al no contener números.
     """
-    context = RegisterTestEnvironment()
+    service = PasswordPolicyService()
+    with pytest.raises(ContraseñaNoSegura):
+        service.validar("Password@asasa")
 
-    usuario = UserRegisterDTO(
-        email="test@test.com",
-        password="Password@asasa"
-    )
+
+def test_debe_dar_error_por_contraseña_sin_mayusculas():
+    """
+    Verifica que la contraseña no cumple con la política de seguridad al no contener mayusculas.
+    """
+    service = PasswordPolicyService()
 
     with pytest.raises(ContraseñaNoSegura):
-        await context.use_case().register(usuario, imagen=None)
+        service.validar("password@asasa123")
+
+def test_debe_dar_error_por_contraseña_sin_minusculas():
+    """
+    Verifica que la contraseña no cumple con la política de seguridad al no contener minusculas.
+    """
+    service = PasswordPolicyService()
+    with pytest.raises(ContraseñaNoSegura):
+        service.validar("PASSWORD@ASASA123")
+
+def test_debe_dar_error_por_contraseña_sin_caracter_especial():
+    """
+    Verifica que la contraseña no cumple con la política de seguridad al no contener caracter especial.
+    """
+    service = PasswordPolicyService()
+    with pytest.raises(ContraseñaNoSegura):
+        service.validar("PASSWORDASASA123")
+
