688a9c82c9bf1f8da41023b5f142721a5865f64d
lazaro20 <salvirialazaro@gmail.com>
2026-07-22 15:01:32 -0300

    Test(src/test/unit/auth/domain/use_case/test_mail_policy.py): Crear un test que verifique que un si un mail es demaciado corto.

---
+++ src/test/unit/auth/domain/use_case/test_mail_policy.py
@@ -0,0 +1,12 @@
+import pytest
+from src.auth.domain.exceptions.domain import MailNoValido
+from src.auth.domain.services.mail_policy import MailPolicyService
+
+
+def test_debe_dar_error_por_mail_corto():
+    """
+    Verifica que el correo no es válido por ser demasiado corto.
+    """
+    service = MailPolicyService()
+    with pytest.raises(MailNoValido):
+        service.validar("a@b.c")
