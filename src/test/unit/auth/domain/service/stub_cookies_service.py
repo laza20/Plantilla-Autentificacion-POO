b982f12774c7117c31c61682569d035b043382e6
lazaro20 <salvirialazaro@gmail.com>
2026-07-23 15:10:32 -0300

    Feat(src/test/unit/auth/domain/service/stub_cookies_service.py): Crear un stub para las cookies.

---
+++ src/test/unit/auth/domain/service/stub_cookies_service.py
@@ -0,0 +1,18 @@
+class StubCookiesService:
+
+    def __init__(self):
+        self.fue_llamado = False
+        self.response = None
+        self.access_token = None
+        self.refresh_token = None
+
+    def set_auth_cookies(
+        self,
+        response,
+        access_token: str,
+        refresh_token: str
+    ):
+        self.fue_llamado = True
+        self.response = response
+        self.access_token = access_token
+        self.refresh_token = refresh_token
