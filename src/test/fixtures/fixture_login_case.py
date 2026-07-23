f3dbaf407b43993842267bd6fa46d74e7e8973ea
lazaro20 <salvirialazaro@gmail.com>
2026-07-23 16:09:54 -0300

    Feat(src/test/fixtures/fixture_login_case.py): Crear una clase la cual instancia el login use case para utilizarlo en los test.

---
+++ src/test/fixtures/fixture_login_case.py
@@ -0,0 +1,32 @@
+from src.test.unit.auth.domain.service import (
+    stub_password_service,
+    stub_token_service
+)
+from src.test.unit.auth.domain.fakes.fake_user_repository import FakeUserRepository
+from src.auth.application.use_cases.login import LoginUseCase
+from src.test.config.config import TestSettings
+from test.unit.auth.domain.service import stub_cookies_service
+from src.auth.domain.services.user_validation_service import UserValidationService
+
+
+class LoginTestEnvironment:
+
+    def __init__(self):
+        self.user_repository = FakeUserRepository()
+        self.password_service = stub_password_service.StubPasswordService()
+        self.token_service = stub_token_service.StubTokenService()
+        self.settings = TestSettings()
+        self.cookies_service = stub_cookies_service.StubCookiesService()
+        self.user_validation_service = UserValidationService(            
+            user_repository=self.user_repository,
+            settings=self.settings)
+
+    def use_case(self):
+        return LoginUseCase(
+            user_repository=self.user_repository,
+            password_service=self.password_service,
+            token_service=self.token_service,
+            settings=self.settings,
+            cookies_service = self.cookies_service,
+            user_validation_service = self.user_validation_service
+        )
