abb937ac339dc89f4d288cab731b7bacd0dbb9c1
lazaro20 <salvirialazaro@gmail.com>
2026-07-23 16:10:40 -0300

    Feat(src/test/unit/auth/domain/service/stub_password_service.py): Crear una funcion que compara el pass dentro del stub de password.

--- src/test/unit/auth/domain/service/stub_password_service.py
+++ src/test/unit/auth/domain/service/stub_password_service.py
@@ -4,3 +4,10 @@ class StubPasswordService:
         Función para simular el hashing de una contraseña.
         """
         return f"hashed_{password}"
+
+
+    def verify_password(self, password: str, hashed_password: str) -> bool:
+        """
+        Función para simular la verificación de una contraseña.
+        """
+        return f"hashed_{password}" == hashed_password
