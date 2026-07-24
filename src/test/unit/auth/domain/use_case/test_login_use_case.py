b15b6a6b0cb224c53bed670ab8ebe7fe5c218416
lazaro20 <salvirialazaro@gmail.com>
2026-07-24 16:49:04 -0300

    Feat(src/test/unit/auth/domain/use_case/test_login_use_case.py): Crear un test principal para login.

---
+++ src/test/unit/auth/domain/use_case/test_login_use_case.py
@@ -0,0 +1,29 @@
+from src.auth.infrastructure.persistence.postgres.models import AuthUser
+from src.test.fixtures.fixture_login_case import LoginTestEnvironment
+from fastapi import Response
+
+
+def test_debe_logear_un_usuario_valido():
+    """
+    Verifica que un usuario pueda iniciar sesión correctamente con datos válidos.
+    """
+    context = LoginTestEnvironment()
+
+    usuario_existente = AuthUser(
+        email="test@test.com",
+        password="hashed_password@123"
+    )
+
+    context.user_repository.insertar(usuario_existente)
+
+
+    cookies = context.use_case().login(
+        email="test@test.com",
+        password="password@123",
+        response=Response()
+    )
+    
+    assert cookies.tokens.access_token == "access_token_1"
+    assert cookies.tokens.refresh_token == "refresh_token_1"
+
+    
