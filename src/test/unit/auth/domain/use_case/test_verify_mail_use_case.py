6f0caf139a381460e71d8872125c14b1f957386f
lazaro20 <salvirialazaro@gmail.com>
2026-07-30 10:53:18 -0300

    Test(src/test/unit/auth/domain/use_case/test_verify_mail_use_case.py): Crear un test el cual verifica que un usuario se activa cuando ser conlleva el caso de uso de verificar mail y que el id utilizado para el mismo es correcto.

---
+++ src/test/unit/auth/domain/use_case/test_verify_mail_use_case.py
@@ -0,0 +1,30 @@
+from src.auth.domain.exceptions.tokens import TokenExpirado, TokenInvalido, VerificacionInvalida, VerificacionExpirada
+import pytest
+from src.test.fixtures.fixture_verify_mail_case import VerifyEmailTestEnvironment
+from src.auth.infrastructure.persistence.postgres.models import AuthUser
+from fastapi import Response
+
+
+def test_debe_verificar_el_mail_y_activar_el_usuario():
+    """
+    Verifica que un usuario pueda activar su cuenta mediante un token válido.
+
+    El caso de uso debe:
+    - verificar que el usuario exista.
+    - validar el token recibido.
+    - marcar al usuario como verificado y activo.
+    - generar nuevos tokens de autenticación.
+    """
+    context = VerifyEmailTestEnvironment()
+
+    usuario_existente = AuthUser(
+        email="test@test.com",
+        password="hashed_password@123"
+    )
+
+    usuario = context.repository.insertar(usuario_existente)
+    tokens = context.use_case().verificar_mail(token= f"access_token_{usuario.id_usuario}", response= Response())
+
+    assert context.token_service.fue_llamado is True
+    assert context.token_service.user_id_recibido == usuario.id_usuario
+
