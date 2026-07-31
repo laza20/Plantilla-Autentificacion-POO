9071d893bbe20d7a6227fb2944da3b5eef5f6d48
lazaro20 <salvirialazaro@gmail.com>
2026-07-31 15:23:26 -0300

    Test(src/test/unit/auth/domain/use_case/test_refresh_token_service.py): Crear el test de caso positivo de refresh token.

--- src/test/unit/auth/domain/use_case/test_refresh_token_service.py
+++ src/test/unit/auth/domain/use_case/test_refresh_token_service.py
@@ -2,6 +2,7 @@ import pytest
 from src.test.fixtures.fixture_refresh_token_case import RefreshTokenTestEnvironment
 from src.auth.infrastructure.persistence.postgres.models import AuthUser
 from src.test.unit.auth.domain.fakes.fake_user_repository import FakeUserRepository
+from fastapi import Response
 
 def test_debe_verificar_el_flujo_correcto_del_refresh():
     context_refresh = RefreshTokenTestEnvironment()
@@ -12,6 +13,11 @@ def test_debe_verificar_el_flujo_correcto_del_refresh():
         password="hashed_password@123"
     )
 
-    context_repository.user_repository.insertar(usuario_existente)
+    usuario = context_repository.insertar(usuario_existente)
 
-    context_refresh.use_case.refreshed_token()
+    context_refresh.use_case().refreshed_token(refresh_token=f"refresh_token_{usuario.id_usuario}",
+            response=Response())
+        
+    assert context_refresh.token_service.user_id_recibido == usuario.id_usuario
+    assert context_refresh.cookies_service.fue_llamado is True
+    assert context_refresh.cookies_service.access_token == context_refresh.token_service.access_token_generado
