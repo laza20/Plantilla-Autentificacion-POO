f5f46db1f8fc14ee11d11cd70f842e59ffc4263f
lazaro20 <salvirialazaro@gmail.com>
2026-08-02 14:55:36 -0300

    Test(src/test/unit/auth/domain/use_case/test_refresh_token_service.py): Crear un test el cual se encarga de probar que las diferentes posibilidades de errores dentro del envio del refresh_token y que todas estas generen un error.

--- src/test/unit/auth/domain/use_case/test_refresh_token_service.py
+++ src/test/unit/auth/domain/use_case/test_refresh_token_service.py
@@ -3,6 +3,7 @@ from src.test.fixtures.fixture_refresh_token_case import RefreshTokenTestEnviron
 from src.auth.infrastructure.persistence.postgres.models import AuthUser
 from src.test.unit.auth.domain.fakes.fake_user_repository import FakeUserRepository
 from fastapi import Response
+from src.auth.domain.exceptions.tokens import VerificacionInvalida
 
 def test_debe_verificar_el_flujo_correcto_del_refresh():
     context_refresh = RefreshTokenTestEnvironment()
@@ -21,3 +22,30 @@ def test_debe_verificar_el_flujo_correcto_del_refresh():
     assert context_refresh.token_service.user_id_recibido == usuario.id_usuario
     assert context_refresh.cookies_service.fue_llamado is True
     assert context_refresh.cookies_service.access_token == context_refresh.token_service.access_token_generado
+
+@pytest.mark.parametrize(
+    "token",
+    [
+        "",
+        "hola",
+        "refresh",
+        "refresh_token",
+        "access_token_1",
+        "token_hola_1",
+        "hola_token_1",
+        "_refresh_token_1",
+        "refresh_token_",
+    ],
+)
+def test_debe_dar_error_cuando_el_formato_del_token_es_invalido(token):
+    """
+    Verifica que cualquier token con un formato inválido produzca
+    una excepción VerificacionInvalida.
+    """
+    context = RefreshTokenTestEnvironment()
+
+    with pytest.raises(VerificacionInvalida):
+        context.use_case().refreshed_token(
+            refresh_token=token,
+            response=Response(),
+        )
