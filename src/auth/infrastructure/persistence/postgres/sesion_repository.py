2f64e58de2ea072d1ad0c353104bb6e9b1e541b9
lazaro20 <salvirialazaro@gmail.com>
2026-08-03 16:32:07 -0300

    Feat(src/auth/infrastructure/persistence/postgres/sesion_repository.py): Crear una funcion que se encarga de insertar una sesion.

---
+++ src/auth/infrastructure/persistence/postgres/sesion_repository.py
@@ -0,0 +1,27 @@
+from auth.infrastructure.persistence.postgres.models_sesiones import SesionesNoTable
+from sqlalchemy.orm import Session
+
+
+class SesionRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def insertar_sesion(
+        self,
+        hash_token: str,
+        id_usuario: int,
+        ip: str,
+        user_agent: str):
+        """
+        Función para insertar un registro en la base de datos y retornar la sesión actualizada.
+        """
+        sesion = SesionesNoTable(
+            hash_token=hash_token,
+            id_usuario=id_usuario,
+            ip=ip,
+            user_agent=user_agent
+        )
+        self.session.add(sesion)
+        self.session.commit()
+        self.session.refresh(sesion)
+        return sesion
