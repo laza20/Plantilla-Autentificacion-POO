f3bd462a1d2aac3faacc050b0cbcafe4e10f870c
lazaro20 <salvirialazaro@gmail.com>
2026-08-03 13:45:53 -0300

    Feat(src/auth/infrastructure/persistence/postgres/models_sesiones.py): Crear un modelo el cual se encarga de las sesiones de los usuairos.

---
+++ src/auth/infrastructure/persistence/postgres/models_sesiones.py
@@ -0,0 +1,32 @@
+from typing import Optional,  TYPE_CHECKING
+from sqlmodel import SQLModel, Field, DateTime, Relationship
+from datetime import datetime
+from sqlalchemy import Column, text
+
+if TYPE_CHECKING:
+    from src.auth.infrastructure.persistence.postgres.models_auth_users import AuthUser
+
+class Sesiones(SQLModel, table=True):
+    __tablename__ = "sesiones"
+
+    id_sesion: Optional[int] = Field(default=None, primary_key=True)
+    
+    hash_refresh_token: str = Field(unique=True, index=True, nullable=False, max_length=64)
+    user_agent: str = Field(default=None, max_length=254)
+    ip: str = Field(default=None, max_length=254)
+    inicio_sesion: Optional[datetime] = Field(default=None, sa_column=Column(DateTime ,server_default=text("CURRENT_TIMESTAMP")))
+    id_usuario: int = Field(foreign_key="auth_users.id_usuario",nullable=False)
+
+
+    usuario: Optional["AuthUser"] = Relationship(
+        back_populates="sesiones"
+    )
+
+
+class SesionesNoTable(SQLModel):
+    id_sesion: Optional[int] = Field(default=None, primary_key=True)
+    hash_refresh_token: str = Field(unique=True, index=True, nullable=False, max_length=64)
+    user_agent: str = Field(default=None, max_length=254)
+    ip: str = Field(default=None, max_length=254)
+    inicio_sesion: Optional[datetime] = Field(default=None, sa_column=Column(DateTime ,server_default=text("CURRENT_TIMESTAMP")))
+    id_usuario: int = Field(foreign_key="auth_users.id_usuario",nullable=False)
