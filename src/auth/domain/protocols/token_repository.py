3ac4616307ecf87e1baee894651cc27ca13cb7ff
lazaro20 <salvirialazaro@gmail.com>
2026-08-03 16:17:39 -0300

    Fix(src/auth/domain/protocols/token_repository.py): Corregir un error, los metodos no son asincronos.

--- src/auth/domain/protocols/token_repository.py
+++ src/auth/domain/protocols/token_repository.py
@@ -4,7 +4,7 @@ from auth.infrastructure.persistence.postgres.models_sesiones import SesionesNoT
 
 @runtime_checkable
 class TokenRepositoryProtocol(Protocol):
-    async def insertar_sesion(
+    def insertar_sesion(
         self,
         hash_token: str,
         id_usuario: int,
@@ -12,20 +12,20 @@ class TokenRepositoryProtocol(Protocol):
         user_agent: str
     ) -> None: ...
 
-    async def listar_sesiones(
+    def listar_sesiones(
         self,
         id_usuario: int,
         ip: str
     ) -> list[Sesion]: ...
 
-    async def eliminar_sesion(
+    def eliminar_sesion(
         self,
         id_sesion: int,
         id_usuario: int
     ) -> None: ...
 
 
-    async def eliminar_por_hash(
+    def eliminar_por_hash(
         self,
         hash_token: str
     ) -> None:...
