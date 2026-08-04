d966616a6dde5247f02136fbed9cc28bd5fd5b5e
lazaro20 <salvirialazaro@gmail.com>
2026-08-04 16:01:32 -0300

    Feat(src/auth/domain/protocols/user_repository.py): Crear un nuevo protocolo el cual permite que el usuario registrado pueda contar con mas datos.

---
+++ src/auth/domain/protocols/user_repository.py
@@ -0,0 +1,7 @@
+from typing import Protocol, runtime_checkable
+from src.auth.infrastructure.persistence.postgres.models_usuario import Usuario
+
+@runtime_checkable
+class UsuarioRepositoryProtocol(Protocol):
+    
+    def insertar(self, usuario: Usuario) -> Usuario: ...
