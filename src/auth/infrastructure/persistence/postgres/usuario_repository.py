32e087f498ade8cd5f1b4cfa5652552fa3c79497
lazaro20 <salvirialazaro@gmail.com>
2026-08-04 16:02:14 -0300

    Faet(src/auth/infrastructure/persistence/postgres/usuario_repository.py): Utilizar el repository el cual permitira usar el protocolo.

---
+++ src/auth/infrastructure/persistence/postgres/usuario_repository.py
@@ -0,0 +1,15 @@
+from sqlmodel import Session
+from src.auth.infrastructure.persistence.postgres.models_usuario import Usuario
+
+class UserRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def insertar(self, usuario:Usuario)-> Usuario:
+        """
+        Función para insertar un registro en la base de datos y retornar el usuario actualizado.
+        """
+        self.session.add(usuario)
+        self.session.commit()
+        self.session.refresh(usuario)
+        return usuario
