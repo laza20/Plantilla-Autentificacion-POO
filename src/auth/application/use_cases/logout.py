4c44b40701635a1d79cc77f2e8ef066e3ececda4
lazaro20 <salvirialazaro@gmail.com>
2026-08-05 15:27:00 -0300

    Feat(src/auth/application/use_cases/logout.py): Agregar a la cadena una serie de pasos los cuales permiten eliminar una sesion.

--- src/auth/application/use_cases/logout.py
+++ src/auth/application/use_cases/logout.py
@@ -1,16 +1,23 @@
 from fastapi import Response
 from src.auth.presentation.web.cookies.cookies import CookiesService
+from src.auth.domain.protocols.token_service import TokenProtocol
+from src.auth.domain.protocols.token_repository import TokenRepositoryProtocol
 
 class LogoutUseCase:
     def __init__(
         self,
-        cookies_service: CookiesService
+        cookies_service: CookiesService,
+        token_service: TokenProtocol,
+        token_repository: TokenRepositoryProtocol
     ):
         self.cookies_service = cookies_service
+        self.token_service = token_service
+        self.token_repository = token_repository
 
-    def logout(self, response:Response):
+    def logout(self, refresh_token: str, response: Response):
         """
         Servicio para deslogear a un usuario.
         """
+        hashed_token = self.token_service.hash_token(refresh_token)
+        self.token_repository.eliminar_por_hash(hashed_token)
         self.cookies_service.delete_auth_cookies(response)
-
