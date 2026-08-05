5d28a193631b1a9995f4efaa465c21ba566466cc
lazaro20 <salvirialazaro@gmail.com>
2026-08-05 10:54:10 -0300

    Feat(src/test/fixtures/fixture_login_case.py): Agregar el request y el response al fixture para poder utilizarlos de manera repetitiva.

--- src/test/fixtures/fixture_login_case.py
+++ src/test/fixtures/fixture_login_case.py
@@ -2,7 +2,9 @@ from src.test.unit.auth.domain.service import (
     stub_password_service,
     stub_token_service
 )
+from fastapi import Response, Request
 from src.test.unit.auth.domain.fakes.fake_auth_user_repository import FakeUserRepository
+from src.test.unit.auth.domain.fakes.fake_token_repository import FakeSesionRepository
 from src.auth.application.use_cases.login import LoginUseCase
 from src.test.config.config import TestSettings
 from test.unit.auth.domain.service import stub_cookies_service
@@ -12,11 +14,24 @@ from src.auth.domain.services.user_validation_service import UserValidationServi
 class LoginTestEnvironment:
 
     def __init__(self):
+        self.request = Request(
+            {
+                "type": "http",
+                "method": "POST",
+                "path": "/login",
+                "headers": [
+                    (b"user-agent", b"pytest"),
+                ],
+                "client": ("127.0.0.1", 50000),
+            }
+        )
+        self.response = Response()
         self.auth_user_repository = FakeUserRepository()
         self.password_service = stub_password_service.StubPasswordService()
         self.token_service = stub_token_service.StubTokenService()
         self.settings = TestSettings()
         self.cookies_service = stub_cookies_service.StubCookiesService()
+        self.token_repository = FakeSesionRepository()
         self.user_validation_service = UserValidationService(            
             auth_user_repository=self.auth_user_repository,
             settings=self.settings)
@@ -28,5 +43,9 @@ class LoginTestEnvironment:
             token_service=self.token_service,
             settings=self.settings,
             cookies_service = self.cookies_service,
+            token_repository = self.token_repository,
             user_validation_service = self.user_validation_service
         )
+
+
+    
