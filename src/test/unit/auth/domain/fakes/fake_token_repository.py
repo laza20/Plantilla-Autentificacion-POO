fa9d76dcd53a7449d7858d8fa1cc5f3c33d9e6ca
lazaro20 <salvirialazaro@gmail.com>
2026-08-05 10:47:40 -0300

    Feat(src/test/unit/auth/domain/fakes/fake_token_repository.py): Crear un nuevo fake, para la utilizacion del token de sesion (token repository) dentro de login en los test.

---
+++ src/test/unit/auth/domain/fakes/fake_token_repository.py
@@ -0,0 +1,30 @@
+from src.auth.infrastructure.persistence.postgres.models_sesiones import Sesiones
+from typing import Dict
+
+
+class FakeSesionRepository:
+    def __init__(self):
+        self._sesiones: Dict[str, Sesiones] = {}
+        self.fue_llamado: bool = False
+        self.hash_refresh_token: str = ""
+
+    def insertar_sesion(
+        self,
+        hash_token: str,
+        id_usuario: int,
+        ip: str,
+        user_agent: str):
+        """
+        Función para insertar un registro en la base de datos y retornar la sesión actualizada.
+        """
+        sesion = Sesiones(
+            hash_refresh_token=hash_token,
+            id_usuario=id_usuario,
+            ip=ip,
+            user_agent=user_agent
+        )
+        copia_sesion = sesion.model_copy()
+        self._sesiones[hash_token] = copia_sesion
+        self.fue_llamado = True
+        self.hash_refresh_token = hash_token
+        return copia_sesion
