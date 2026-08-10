c9386760bffef0e3b0742b8b1b1e584dd1701899
lazaro20 <salvirialazaro@gmail.com>
2026-08-10 14:58:58 -0300

    Test(src/test/unit/auth/domain/use_case/test_listar_sesiones_use_case.py): Crear un test el cual se encarga de listar mas de una sesion, sin la sesion del ip actual entre ellas.

--- src/test/unit/auth/domain/use_case/test_listar_sesiones_use_case.py
+++ src/test/unit/auth/domain/use_case/test_listar_sesiones_use_case.py
@@ -30,3 +30,51 @@ def test_listar_sesiones_usuario_sin_sesiones():
     sesiones = contex_listar_sesion.use_case().ejecutar(id_usuario=2, ip="127.0.0.1")
 
     assert len(sesiones.sesiones) == 0
+
+def test_debe_listar_varias_sesiones_y_marcar_la_actual():
+    """
+    Verifica que se puedan listar varias sesiones de un usuario y marcar la sesión actual.
+    """
+    contex_listar_sesion = ListarSesionesEnvironment()
+
+    contex_listar_sesion.token_repository.insertar_sesion(
+        hash_token="hash_token_1",
+        id_usuario=1,
+        ip="127.0.0.1",
+        user_agent="user_agent_1")
+
+    contex_listar_sesion.token_repository.insertar_sesion(
+        hash_token="hash_token_2",
+        id_usuario=1,
+        ip="128.0.0.1",
+        user_agent="user_agent_2")
+
+    sesiones = contex_listar_sesion.use_case().ejecutar(id_usuario=1, ip="127.0.0.1")
+
+    assert len(sesiones.sesiones) == 2
+    assert sesiones.sesiones[0].es_actual is True
+    assert sesiones.sesiones[1].es_actual is False
+
+
+def test_debe_listar_varias_sesiones_sin_la_ip_actual():
+    """
+    Verifica que se puedan listar varias sesiones de un usuario sin una sesion marcada como actual si la IP no coincide con ninguna sesión.
+    """
+    contex_listar_sesion = ListarSesionesEnvironment()
+
+    contex_listar_sesion.token_repository.insertar_sesion(
+        hash_token="hash_token_1",
+        id_usuario=1,
+        ip="127.0.0.1",
+        user_agent="user_agent_1")
+
+    contex_listar_sesion.token_repository.insertar_sesion(
+        hash_token="hash_token_2",
+        id_usuario=1,
+        ip="128.0.0.1",
+        user_agent="user_agent_2")
+
+    sesiones = contex_listar_sesion.use_case().ejecutar(id_usuario=1, ip="129.0.0.1")
+
+    assert sesiones.sesiones[0].es_actual is False
+    assert sesiones.sesiones[1].es_actual is False
