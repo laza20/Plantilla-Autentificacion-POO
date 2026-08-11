770086be47831952a139488d90c022c40fbf545a
lazaro20 <salvirialazaro@gmail.com>
2026-08-11 09:19:39 -0300

    Refactor(src/auth/application/use_cases/eliminar_sesiones.py): Corregir el nombre del caso de uso.

--- src/auth/application/use_cases/eliminar_sesiones.py
+++ src/auth/application/use_cases/eliminar_sesiones.py
@@ -1,10 +1,10 @@
 from src.auth.domain.protocols.token_repository import TokenRepositoryProtocol
 
 
-class EliminarSesiones:
+class EliminarSesionesUseCase:
     def __init__(self, token_repository: TokenRepositoryProtocol):
         self.token_repository = token_repository
 
-    def eliminar_sesion(self, id_sesion: int, id_usuario: int):
+    def ejecutar(self, id_sesion: int, id_usuario: int):
         self.token_repository.eliminar_sesion(id_sesion=id_sesion, id_usuario=id_usuario)
 
