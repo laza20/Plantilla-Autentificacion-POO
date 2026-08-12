29d5a02cdadc064bdc55ec2976945f0e42982ee3
lazaro20 <salvirialazaro@gmail.com>
2026-08-12 14:05:34 -0300

    Refactor(src/test/unit/auth/domain/use_case/test_logout.py): Utilizar las factories del usuario.

--- src/test/unit/auth/domain/use_case/test_logout.py
+++ src/test/unit/auth/domain/use_case/test_logout.py
@@ -1,5 +1,6 @@
 from fastapi import Response
 from src.test.fixtures.fixture_logout_case import LogoutTestEnvironment
+from test.factories.factory_sesiones import crear_sesion_de_prueba
 
 
 def test_debe_llamar_al_servicio_de_cookies():
@@ -24,14 +25,9 @@ def test_debe_enviar_el_response_al_servicio_de_cookies():
 
 def test_debe_verificar_que_se_elimino_el_token_en_el_repositorio():
     context = LogoutTestEnvironment()
-    use_case = context.use_case()
     response = Response()
-    context.token_repository.insertar_sesion(
-        hash_token="hashed_token_de_prueba",
-        id_usuario=1,
-        ip="127.0.0.1",
-        user_agent="test"
-    )
+    crear_sesion_de_prueba(context.token_repository, hash_token="hashed_token_de_prueba", user_agent="test")
+
     refresh_token = "token_de_prueba"
-    use_case.ejecutar(refresh_token, response)
+    context.use_case().ejecutar(refresh_token, response)
     assert context.token_repository.token_eliminado == context.token_service.hash_token(refresh_token)
