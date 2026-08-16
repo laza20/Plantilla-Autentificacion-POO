036c76b25ae6643a5ccd3f4be074a036f3505394
lazaro20 <salvirialazaro@gmail.com>
2026-08-16 20:51:04 -0300

    Test(src/test/presentation/web/routers/test_router_refresh_token.py): Crear un test el cual se encargue de verificar que cuando no se envia el refresh token en las cookies, se produce un error del tipo SinRefreshToken.

--- src/test/presentation/web/routers/test_router_refresh_token.py
+++ src/test/presentation/web/routers/test_router_refresh_token.py
@@ -2,6 +2,7 @@ from src.main import app
 from src.test.presentation.web.overrides.override_refresh_token import crear_override
 from src.container.providers import get_refresh_token_service
 from src.config.config import settings
+from src.auth.domain.exceptions.usuarios_exceptions import SinRefreshToken
 
 
 def test_debe_refrescar_el_token_correctamente(test_client):
@@ -14,3 +15,14 @@ def test_debe_refrescar_el_token_correctamente(test_client):
 
     assert response.status_code == 200
     assert response.json()["message"] == "Access token renovado"
+
+
+def test_debe_verificar_que_se_produce_una_excepcion_si_no_se_envia_el_refresh_token(test_client):
+    app.dependency_overrides[get_refresh_token_service] = crear_override(excepcion = SinRefreshToken("No hay refresh token en la cookie"))
+
+    response = test_client.post(
+        f"/{settings.NOMBRE_APP}/usuarios/Refresh/Token",
+    )
+
+    assert response.status_code == 401
+    assert response.json()["detail"] == "No hay refresh token en la cookie"
