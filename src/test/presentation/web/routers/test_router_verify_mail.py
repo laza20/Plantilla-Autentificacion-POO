f7832a1792ce5c67266e8274f8fa406f6c9d1ee2
lazaro20 <salvirialazaro@gmail.com>
2026-08-16 20:19:29 -0300

    Test(src/test/presentation/web/routers/test_router_verify_mail.py): Crear un test para el caso negativo (se envia un token invalido), verificar que retorne un error.

--- src/test/presentation/web/routers/test_router_verify_mail.py
+++ src/test/presentation/web/routers/test_router_verify_mail.py
@@ -2,6 +2,8 @@ from src.main import app
 from src.test.presentation.web.overrides.override_verify_mail import crear_override
 from src.container.providers import get_verify_mail_use_case
 from src.config.config import settings
+from src.auth.domain.exceptions.tokens import TokenInvalido, VerificacionInvalida
+import pytest
 
 token = "token_correcto_1"
 
@@ -13,7 +15,16 @@ def test_debe_verificar_de_manera_correcta_un_mail(test_client):
         f"/{settings.NOMBRE_APP}/usuarios/verificar/{token}"
     )
 
-    print(response.json())
-
     assert response.json()["status"] == "success"
     assert response.json()["message"] == "¡Cuenta activada con éxito!"
+
+
+def test_debe_lanzar_error_si_el_token_es_invalido(test_client):
+    app.dependency_overrides[get_verify_mail_use_case] = crear_override(
+        excepcion=TokenInvalido()
+    )
+
+    response = test_client.get(f"/{settings.NOMBRE_APP}/usuarios/verificar/token_invalido")
+
+    assert response.status_code == TokenInvalido().status_code
+    assert response.json()["detail"] == 'Token de ingreso invalido.'
