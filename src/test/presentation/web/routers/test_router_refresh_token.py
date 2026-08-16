2dcc88ab18be2ac9b8eedddfc12e641d5d6ae27b
lazaro20 <salvirialazaro@gmail.com>
2026-08-16 20:59:35 -0300

    Test(src/test/presentation/web/routers/test_router_refresh_token.py): Crear un test el cual se encarga de verificar que se produce una excepcion cuando se envia un token invalido (exception del tipo VerificacionInvalida).

--- src/test/presentation/web/routers/test_router_refresh_token.py
+++ src/test/presentation/web/routers/test_router_refresh_token.py
@@ -3,6 +3,7 @@ from src.test.presentation.web.overrides.override_refresh_token import crear_ove
 from src.container.providers import get_refresh_token_service
 from src.config.config import settings
 from src.auth.domain.exceptions.usuarios_exceptions import SinRefreshToken
+from src.auth.domain.exceptions.tokens import VerificacionInvalida
 
 
 def test_debe_refrescar_el_token_correctamente(test_client):
@@ -26,3 +27,15 @@ def test_debe_verificar_que_se_produce_una_excepcion_si_no_se_envia_el_refresh_t
 
     assert response.status_code == 401
     assert response.json()["detail"] == "No hay refresh token en la cookie"
+
+
+def test_debe_verificar_que_la_verificacion_fue_invalida(test_client):
+    app.dependency_overrides[get_refresh_token_service] = crear_override(excepcion = VerificacionInvalida())
+
+    test_client.cookies.set("refresh_token", "un_token_invalido")
+    response = test_client.post(
+        f"/{settings.NOMBRE_APP}/usuarios/Refresh/Token",
+    )
+    
+    assert response.status_code == VerificacionInvalida().status_code
+    assert response.json()["detail"] == VerificacionInvalida().message
