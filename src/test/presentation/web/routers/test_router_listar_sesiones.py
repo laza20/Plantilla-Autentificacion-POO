73b424c48ba9bf551d9a85e2c109b6c26a33b5ca
lazaro20 <salvirialazaro@gmail.com>
2026-08-19 11:56:41 -0300

    Feat(Router Test Listar Sesiones -- Stub): Crear el camino logico del router, crear un test el cual verifique en el router el caso positivo de listar sesiones.

---
+++ src/test/presentation/web/routers/test_router_listar_sesiones.py
@@ -0,0 +1,46 @@
+import pytest
+from test.presentation.web.overrides.override_crear import crear_override
+from test.presentation.web.stub.stub_listar_sesiones import StubListarSesionesUseCase
+from test.presentation.web.stub.stub_current_user import StubCurrentUserUseCase
+from src.auth.infrastructure.persistence.postgres.models_auth_users import AuthUser
+from src.container.providers import get_user_validation_service, get_listar_sesiones_use_case
+from src.auth.presentation.web.guards import get_current_user
+from fastapi import status
+from src.config.config import settings
+from src.main import app
+
+
+def test_debe_listar_sesiones(test_client):
+    usuario = AuthUser(
+        id_usuario=1,
+        email="test@test.com",
+        password="password@123"
+    )
+
+    sesiones = {
+        "id_sesion":1,
+        "hash_refresh_token":"hash_refresh_token_1",
+        "ip": "localhost",
+        "id_usuario": 1
+    }
+    app.dependency_overrides[get_current_user] = lambda: {"id_usuario": 1, "email": "test@test.com"}
+    app.dependency_overrides[get_user_validation_service] = crear_override(
+        stub_class=StubCurrentUserUseCase, 
+        resultado=usuario
+        )
+
+    app.dependency_overrides[get_listar_sesiones_use_case] = crear_override(
+        stub_class=StubListarSesionesUseCase,
+        resultado=sesiones
+    )
+
+    response = test_client.get(
+        f"/{settings.NOMBRE_APP}/usuarios/listar_sesiones"
+    )
+
+    data = response.json()
+    assert response.status_code == status.HTTP_200_OK
+    assert data["id_sesion"] == sesiones["id_sesion"]
+    assert data["hash_refresh_token"] == sesiones["hash_refresh_token"]
+    assert data["ip"] == sesiones["ip"]
+    assert data["id_usuario"] == sesiones["id_usuario"]
---
+++ src/test/presentation/web/stub/stub_listar_sesiones.py
@@ -0,0 +1,12 @@
+
+
+
+class StubListarSesionesUseCase:
+    def __init__(self):
+        self.resultado_a_devolver = None
+        self.excepcion_a_lanzar = None
+
+    def ejecutar(self, id_usuario, ip):
+        if self.excepcion_a_lanzar:
+            raise self.excepcion_a_lanzar
+        return self.resultado_a_devolver
