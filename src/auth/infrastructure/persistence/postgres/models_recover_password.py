d6a4ea47a031264bc4c95104fb73fcc5f724564a
lazaro20 <salvirialazaro@gmail.com>
2026-08-21 16:32:43 -0300

    Feat(src/auth/infrastructure/persistence/postgres/models_recover_password.py): Crear un modelo de base de datos y uno que sea identico pero no sea de base de datos, para utilizar en el funcionamiento de la validacion y la db.

---
+++ src/auth/infrastructure/persistence/postgres/models_recover_password.py
@@ -0,0 +1,29 @@
+from typing import Optional, TYPE_CHECKING
+from sqlmodel import SQLModel, Field, Relationship, DateTime
+from datetime import datetime
+from sqlalchemy import Column, text
+
+if TYPE_CHECKING:
+    from src.auth.infrastructure.persistence.postgres.models_auth_users import AuthUser
+
+class RecoverPassword(SQLModel, table=True):
+    __tablename__ = "recover_password"
+
+    id_recover_password: Optional[int] = Field(default=None, primary_key=True)
+    
+    token_hash: str = Field(unique=True, index=True, nullable=False, max_length=255)
+    expira_en: Optional[datetime] = Field(default=None, sa_column=Column(DateTime(timezone=True), server_default=text("CURRENT_TIMESTAMP")))
+    usado: bool = Field(default=False)
+    id_usuario: int = Field(foreign_key="auth_users.id_usuario",nullable=False)
+
+    usuario: Optional["AuthUser"] = Relationship(
+        back_populates="recover_password"
+    )
+
+
+class RecoverPasswordNoTable(SQLModel):
+    id_recover_password: Optional[int] = Field(default=None, primary_key=True)
+    token_hash: str = Field(unique=True, index=True, nullable=False, max_length=255)
+    expira_en: Optional[datetime] = Field(default=None, sa_column=Column(DateTime(timezone=True), server_default=text("CURRENT_TIMESTAMP")))
+    usado: bool = Field(default=False)
+    id_usuario: int = Field(foreign_key="auth_users.id_usuario",nullable=False)
