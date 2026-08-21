4e4dbb6c651d24977309fee9f3470aaac1da5230
lazaro20 <salvirialazaro@gmail.com>
2026-08-21 16:31:37 -0300

    Feat(src/auth/infrastructure/persistence/postgres/models_history_password.py): Crear un modelo de base de datos de history password. Crear un modelo identico al de db de history password, pero que no sea de base de datos para validar.

---
+++ src/auth/infrastructure/persistence/postgres/models_history_password.py
@@ -0,0 +1,29 @@
+from typing import Optional, TYPE_CHECKING
+from sqlmodel import SQLModel, Field, Relationship, DateTime
+from datetime import datetime
+from sqlalchemy import Column, text
+
+if TYPE_CHECKING:
+    from src.auth.infrastructure.persistence.postgres.models_auth_users import AuthUser
+
+class HisoryPassword(SQLModel, table=True):
+    __tablename__ = "history_password"
+
+    id_history_password: Optional[int] = Field(default=None, primary_key=True)
+    
+    password_hash_anterior: str = Field(unique=True, index=True, nullable=False, max_length=255)
+    fecha_cambio: Optional[datetime] = Field(default=None, sa_column=Column(DateTime(timezone=True), server_default=text("CURRENT_TIMESTAMP")))
+    id_usuario: int = Field(foreign_key="auth_users.id_usuario",nullable=False)
+
+    usuario: Optional["AuthUser"] = Relationship(
+        back_populates="history_password"
+    )
+
+
+
+class HisoryPasswordNoTable(SQLModel):
+    id_history_password: Optional[int] = Field(default=None, primary_key=True)
+    password_hash_anterior: str = Field(unique=True, index=True, nullable=False, max_length=255)
+    fecha_cambio: Optional[datetime] = Field(default=None, sa_column=Column(DateTime(timezone=True), server_default=text("CURRENT_TIMESTAMP")))
+    id_usuario: int = Field(foreign_key="auth_users.id_usuario",nullable=False)
+
