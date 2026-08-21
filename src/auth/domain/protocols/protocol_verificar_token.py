515c70df55939b3d08afa3f20cd986d0bc070a81
lazaro20 <salvirialazaro@gmail.com>
2026-08-21 16:27:18 -0300

    Refactor: Modificar el protocolo de verificacion a history_password_protocol.

--- src/auth/domain/protocols/protocol_verificar_token.py
+++
@@ -1,6 +0,0 @@
-from typing import Protocol, runtime_checkable
-from datetime import datetime
-
-@runtime_checkable
-class VerificarTokenProtocol(Protocol):
-  def verificar_token(self, token_recuperacion:str, id_usuario:int, fecha_actual:datetime)->bool:pass
