750fbba392c7ad9767c9711c44ec0ac2d581bc66
lazaro20 <salvirialazaro@gmail.com>
2026-08-27 16:33:46 -0300

    Feat(src/auth/infrastructure/persistence/postgres/history_password_repository.py): Crear el metodo de contrato el cual permite guardar un registro dentro del sistema.

--- src/auth/infrastructure/persistence/postgres/history_password_repository.py
+++ src/auth/infrastructure/persistence/postgres/history_password_repository.py
@@ -1,4 +1,4 @@
-from src.auth.infrastructure.persistence.postgres.models_sesiones import Sesiones
+from src.auth.infrastructure.persistence.postgres.models_history_password import HistoryPassword
 from sqlalchemy.orm import Session
 from datetime import datetime
 
@@ -8,3 +8,16 @@ class HistoryPasswordRepository:
         self.session = session
 
 
+    def insertar_history_repository(
+        self, 
+        id_usuario:int, 
+        password_hash_anterior:str, 
+        fecha_cambio:datetime)->bool:
+
+        history_password = HistoryPassword(
+            id_usuario=id_usuario,
+            fecha_cambio=fecha_cambio,
+            password_hash_anterior=password_hash_anterior
+        )
+        self.session.add(history_password)
+        return True
