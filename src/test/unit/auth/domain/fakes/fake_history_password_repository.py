1bcf86d984c380d3cc1e46d608a4adea37219e26
lazaro20 <salvirialazaro@gmail.com>
2026-08-31 15:37:29 -0300

    Feat(src/test/unit/auth/domain/fakes/fake_history_password_repository.py): Recrear el repositorio del history password repositorio. Con todos los metodos de contrato que estos deben seguir.

---
+++ src/test/unit/auth/domain/fakes/fake_history_password_repository.py
@@ -0,0 +1,27 @@
+from src.auth.infrastructure.persistence.postgres.models_history_password import HistoryPassword
+from datetime import datetime
+
+class FakeHistoryPasswordRepository:
+    def __init__(self):
+        self.history_password: dict[str, HistoryPassword] = {}
+        self.fue_llamado: bool = False
+        self._next_history_password_id= 1
+
+    def insertar_history_repository(
+        self, 
+        id_usuario:int, 
+        password_hash_anterior:str, 
+        fecha_cambio:datetime)->bool:
+
+        history_password = HistoryPassword(
+            id_usuario=id_usuario,
+            fecha_cambio=fecha_cambio,
+            password_hash_anterior=password_hash_anterior
+        )
+
+        history_password.id_history_password = self._next_history_password_id
+        copia_history_password = history_password.model_copy()
+        self.history_password[id_usuario] = copia_history_password
+        self._next_recover_id += 1
+        self.fue_llamado = True
+        return True
