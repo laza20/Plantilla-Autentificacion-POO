63d9b7dd0f7eb2fbc7f93bd3568f88f1f4db5666
lazaro20 <salvirialazaro@gmail.com>
2026-09-01 10:38:20 -0300

    Test(src/test/unit/auth/domain/use_case/test_verificar_token_recuperar_contraseña.py): Crear un test el cual se encarga de verificar que al pasarle un token incorrecto (no es el mismo token que presisa para el verificar), se genera un error de tipo TokenInvalido.

--- src/test/unit/auth/domain/use_case/test_verificar_token_recuperar_contraseña.py
+++ src/test/unit/auth/domain/use_case/test_verificar_token_recuperar_contraseña.py
@@ -1,5 +1,7 @@
 from src.test.fixtures.fixture_verificar_token_recuperacion_contraseña import VerificarTokenRecuperacionContraseñaTestEnvironment
 from src.test.factories.factory_create_password_recover import crear_recover_password_de_prueba
+from src.auth.domain.exceptions.tokens import TokenNoVerificado
+import pytest
 
 
 
@@ -16,3 +18,16 @@ def test_debe_verificar_que_token_service_fue_llamado_y_funciona_correctamente()
 
     assert context.token_service.fue_llamado == True
     assert context.token_service.hash_token == "hashed_1a2s3w5e6g9s8d5c5d6s5c5s5d5s69s7"
+
+def test_debe_verificar_que_al_pasarse_un_token_invalido_genera_un_error_de_tipo_TokenInvalido():
+    """
+    El test se encarga de verificar que en el caso de que se pase un token invalido, el caso de uso
+    genera un error de tipo TokenInvalido.
+    """
+    context = VerificarTokenRecuperacionContraseñaTestEnvironment()
+    sesion_insertada = crear_recover_password_de_prueba(context.recuperar_contraseña_repository)
+
+    with pytest.raises(TokenNoVerificado):
+        context.use_case().ejecutar(
+            "1a2s3w5e6g9s" #token invalido
+        )
