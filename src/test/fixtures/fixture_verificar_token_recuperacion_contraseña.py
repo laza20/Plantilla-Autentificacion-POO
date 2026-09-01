c234e5f28a8195790aaff0da383a57cf34040d45
lazaro20 <salvirialazaro@gmail.com>
2026-09-01 10:15:43 -0300

    Fix(src/test/fixtures/fixture_verificar_token_recuperacion_contraseña.py): Corregir la llamada al caso de uso, por equivocacion de archivos.

--- src/test/fixtures/fixture_verificar_token_recuperacion_contraseña.py
+++ src/test/fixtures/fixture_verificar_token_recuperacion_contraseña.py
@@ -2,14 +2,14 @@ from src.test.unit.auth.domain.service import (
     stub_token_service
 )
 from src.test.unit.auth.domain.fakes.fake_recuperar_contraseña_repository import FakeRecuperarContraseñaRepository
-from src.auth.application.use_cases.recover_password.solicitud_recuperacion import SolicitudRecuperacionUseCase
+from src.auth.application.use_cases.recover_password.verificar_token import VerificarTokenUseCase
 
 class VerificarTokenRecuperacionContraseñaTestEnvironment:
     def __init__(self):
         self.recuperar_contraseña_repository = FakeRecuperarContraseñaRepository()
         self.token_service = stub_token_service.StubTokenService()
     def use_case(self):
-        return SolicitudRecuperacionUseCase(
+        return VerificarTokenUseCase(
             recuperar_contraseña_repository = self.recuperar_contraseña_repository,
             token_service = self.token_service
         )
