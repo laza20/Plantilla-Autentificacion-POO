a30842e6885087757ba34a0e06aaea385b429dfc
lazaro20 <salvirialazaro@gmail.com>
2026-09-02 13:21:49 -0300

    Feat(Test router - Reset token): Crear el stub para el verificar token recuperacion contraseña y el archivo principal para testear. Crear el test el cual se encarga de verificar que la respuesta sea la esperada.

---
+++ src/test/presentation/web/routers/test_router_verificar_token_recuperar_cotraseña.py
@@ -0,0 +1,27 @@
+from src.main import app
+from src.test.presentation.web.overrides.override_crear import crear_override
+from src.test.presentation.web.stub.stub_verificar_token_recuperar_contraseña import StubVerificarTokenRecuperarContraseñaUseCase
+from fastapi import status
+from src.container.providers import get_verificar_token_recuperacion_contraseña_use_case
+from src.config.config import settings
+from src.auth.domain.exceptions.tokens import TokenNoVerificado
+
+
+def test_debe_verificar_el_retorno_del_reset_token_correctamente(test_client):
+    token = "un_token_valido"
+    token_retorno = "un_nuevo_token"
+    app.dependency_overrides[get_verificar_token_recuperacion_contraseña_use_case] = crear_override(
+        stub_class=StubVerificarTokenRecuperarContraseñaUseCase,
+        resultado=token_retorno
+        )
+
+    response = test_client.get(
+        f"/{settings.NOMBRE_APP}/usuarios/recuperar/password/{token}",
+    )
+
+    data = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert data["message"] == 'Configuracion realizada correctamente.'
+
+
---
+++ src/test/presentation/web/stub/stub_verificar_token_recuperar_contraseña.py
@@ -0,0 +1,12 @@
+
+
+
+class StubVerificarTokenRecuperarContraseñaUseCase:
+    def __init__(self):
+        self.resultado_a_devolver = None
+        self.excepcion_a_lanzar = None
+
+    def ejecutar(self, token:str):
+        if self.excepcion_a_lanzar:
+            raise self.excepcion_a_lanzar
+        return self.resultado_a_devolver
