c70c565329baa84dc9b70b605d5056840348b7da
lazaro20 <salvirialazaro@gmail.com>
2026-09-11 16:49:43 -0300

    Feat(src/auth/infrastructure/persistence/postgres/schemas/schemas_recepcion.py): Crear un schema para la recepcion de datos para el caso de uso de reactivacion de mail.

--- src/auth/infrastructure/persistence/postgres/schemas/schemas_recepcion.py
+++ src/auth/infrastructure/persistence/postgres/schemas/schemas_recepcion.py
@@ -3,6 +3,9 @@ from pydantic import BaseModel, EmailStr
 class SolicitudRecuperacionRequest(BaseModel):
     email: EmailStr
 
+class SolicitudReactivacionRequest(BaseModel):
+    email: EmailStr
+
 
 class ModificarPassword(BaseModel):
     password: str
