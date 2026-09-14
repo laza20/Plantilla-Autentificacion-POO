41df0952126753c0ce4049113681059f95e3ec41
lazaro20 <salvirialazaro@gmail.com>
2026-09-14 16:39:27 -0300

    Feat(src/test/unit/auth/domain/use_case/test_reenviar_mail.py): Crear un test el cual verifica si se produce un error el cual no se encuentra un usuario.

--- src/test/unit/auth/domain/use_case/test_reenviar_mail.py
+++ src/test/unit/auth/domain/use_case/test_reenviar_mail.py
@@ -21,3 +21,10 @@ async def test_debe_verificar_que_se_llama_a_token_service():
 
     resultado = await context.use_case().ejecutar("test@test.com")
     assert context.token_service.verificacion_token_generado == f"verificacion_token_1"
+
+@pytest.mark.asyncio
+async def test_debe_dar_error_cuando_el_usuario_no_existe():
+    context = ReenviarMailTestEnvironment()
+    
+    with pytest.raises(UsuarioNoEncontrado):
+        await context.use_case().ejecutar("te@test.com")
