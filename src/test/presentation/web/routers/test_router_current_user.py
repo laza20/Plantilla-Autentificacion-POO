2530c144d98e45a21acd502bd334b46cbce02637
lazaro20 <salvirialazaro@gmail.com>
2026-09-15 13:36:55 -0300

    Fix(Test Routers): Corregir la llamada a las dependencias de current user.

--- src/test/presentation/web/routers/test_router_current_user.py
+++ src/test/presentation/web/routers/test_router_current_user.py
@@ -2,8 +2,7 @@ import pytest
 from src.test.presentation.web.overrides.override_crear import crear_override
 from src.test.presentation.web.stub.stub_current_user import StubCurrentUserUseCase
 from src.auth.infrastructure.persistence.postgres.models.models_auth_users import AuthUser
-from src.container.providers import get_user_validation_service
-from src.auth.presentation.web.guards import get_current_user
+from src.container.providers import get_user_validation_service, get_current_user
 from fastapi import status
 from src.config.config import settings
 from src.main import app
@@ -25,11 +24,9 @@ def test_ver_usuario_correctamente(test_client):
         )
 
     response = test_client.get(f"/{settings.NOMBRE_APP}/usuarios/user/current")
-  
+
     assert response.status_code == status.HTTP_200_OK
-    assert response.json()["id_usuario"] == usuario_esperado.id_usuario
     assert response.json()["email"] == usuario_esperado.email
-    assert response.json()["password"] == usuario_esperado.password
 
 
 def test_debe_error_si_el_usuario_no_esta_autenticado(test_client):
