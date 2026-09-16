16970aa4e25411fa0150f5fa630a8101e3c97630
lazaro20 <salvirialazaro@gmail.com>
2026-09-16 15:51:23 -0300

    Fix(src/test/unit/auth/domain/use_case/test_reactivar_cuenta.py): Corregir cuando se verifica el tiempo por el error PeriodoReactivacionFinalizado.

--- src/test/unit/auth/domain/use_case/test_reactivar_cuenta.py
+++ src/test/unit/auth/domain/use_case/test_reactivar_cuenta.py
@@ -8,6 +8,7 @@ from src.auth.domain.exceptions.usuarios_exceptions import (
 from src.auth.domain.exceptions.tokens import TokenInvalido
 from src.auth.domain.exceptions.domain import ErrorCreacion
 from src.test.factories.factory_usuarios import crear_usuario_de_prueba
+from datetime import date, timedelta
 
 
 @pytest.mark.asyncio
@@ -62,3 +63,16 @@ async def test_debe_verificar_que_se_llamo_al_token_service():
 
     resultado = context.use_case().ejecutar(token=f"reactivacion_token_{usuario_creado.id_usuario}")
     assert context.token_service.fue_llamado == True
+
+
+@pytest.mark.asyncio
+async def test_debe_verificar_al_ser_un_tiempo_posterior_al_posible_da_un_error_de_periodo_de_inactividad():
+    context = ReactivarCuentaTestEnvironment()
+    usuario_creado = crear_usuario_de_prueba(context.auth_user_repository)
+    context.auth_user_repository.tiempo_eliminado = date.today() - timedelta(days=60)
+    context.auth_user_repository.eliminar_usuario(usuario_creado)
+
+    with pytest.raises(PeriodoReactivacionFinalizado):
+        context.use_case().ejecutar(
+            token=f"reactivacion_token_{usuario_creado.id_usuario}"
+        )
