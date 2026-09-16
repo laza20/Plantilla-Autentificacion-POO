4a57deeb1a917e1bda28a41784bfb17e0b5f217e
lazaro20 <salvirialazaro@gmail.com>
2026-09-16 13:36:44 -0300

    Test(src/test/unit/auth/domain/use_case/test_solicitud_reactivacion_cuenta.py): Crear 2 test los cuales se encargan de verificar que verificar que se llaman a los servicios.

--- src/test/unit/auth/domain/use_case/test_solicitud_reactivacion_cuenta.py
+++ src/test/unit/auth/domain/use_case/test_solicitud_reactivacion_cuenta.py
@@ -16,3 +16,32 @@ async def test_verifica_que_se_solicita_una_reactivacion_de_cuenta_correctamente
     resultado = await context.use_case().ejecutar(usuario_creado.email)
     assert resultado["message"] == "Correo enviado correctamente"
 
+@pytest.mark.asyncio
+async def test_verifica_que_se_llamo_al_mail_service():
+    context = SolicitudReactivacionCuentaTestEnvironment()
+    usuario_creado = crear_usuario_de_prueba(context.auth_user_repository)
+    context.auth_user_repository.eliminar_usuario(usuario_creado)
+
+    await context.use_case().ejecutar(usuario_creado.email)
+    assert context.mail_service.fue_llamado != False
+
+
+@pytest.mark.asyncio
+async def test_verifica_que_se_llamo_al_token_service():
+    context = SolicitudReactivacionCuentaTestEnvironment()
+    usuario_creado = crear_usuario_de_prueba(context.auth_user_repository)
+    context.auth_user_repository.eliminar_usuario(usuario_creado)
+
+    await context.use_case().ejecutar(usuario_creado.email)
+    assert context.token_service.fue_llamado != False
+
+
+@pytest.mark.asyncio
+async def test_verifica_que_se_llamo_al_token_service():
+    context = SolicitudReactivacionCuentaTestEnvironment()
+    usuario_creado = crear_usuario_de_prueba(context.auth_user_repository)
+    context.auth_user_repository.eliminar_usuario(usuario_creado)
+
+    await context.use_case().ejecutar(usuario_creado.email)
+    assert context.token_service.fue_llamado != False
+    assert context.token_service.reactivacion_token_generado == f"reactivacion_token_{usuario_creado.id_usuario}"
