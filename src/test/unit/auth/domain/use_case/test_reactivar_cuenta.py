02edb3e07115e1a8f0fd54a86beeb958d584fddd
lazaro20 <salvirialazaro@gmail.com>
2026-09-16 15:06:09 -0300

    Test(src/test/unit/auth/domain/use_case/test_reactivar_cuenta.py): Crear un test el cual se encarga de verificar que al pasarse un token incorrecto, el mismo es invalido y genera una excepcion.

--- src/test/unit/auth/domain/use_case/test_reactivar_cuenta.py
+++ src/test/unit/auth/domain/use_case/test_reactivar_cuenta.py
@@ -5,6 +5,7 @@ from src.test.fixtures.fixture_reactivar_cuenta import ReactivarCuentaTestEnviro
 from src.auth.domain.exceptions.usuarios_exceptions import (
     UsuarioNoEncontrado, UsuarioNoEliminado, PeriodoReactivacionFinalizado
     )
+from src.auth.domain.exceptions.tokens import TokenInvalido
 from src.auth.domain.exceptions.domain import ErrorCreacion
 from src.test.factories.factory_usuarios import crear_usuario_de_prueba
 
@@ -17,3 +18,28 @@ async def test_verifica_que_se_reactiva_una_cuenta():
 
     resultado = context.use_case().ejecutar(token=f"reactivacion_token_{usuario_creado.id_usuario}")
     assert resultado["message"] == "La cuenta fue reactivada correctamente."
+
+@pytest.mark.parametrize(
+    "token",
+    [
+        "",
+        "hola",
+        "reactivacion",
+        "reactivacion_token",
+        "access_token_1",
+        "token_hola_1",
+        "hola_token_1",
+        "_reactivacion_token_1",
+        "reactivacion_token_",
+    ],
+)
+@pytest.mark.asyncio
+async def test_debe_dar_error_por_token_invalido(token):
+    context = ReactivarCuentaTestEnvironment()
+    usuario_creado = crear_usuario_de_prueba(context.auth_user_repository)
+    context.auth_user_repository.eliminar_usuario(usuario_creado)
+
+    with pytest.raises(TokenInvalido):
+        context.use_case().ejecutar(
+            token=token
+        )
